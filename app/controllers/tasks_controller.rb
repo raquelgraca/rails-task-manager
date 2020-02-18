4b4ef74d4f784a26886573296e9d284d9686b22e
raquelgraca <raquelngraca@gmail.com>
2020-02-18 18:49:51 -0300

    Completed action in the controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,42 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
+  end
+
+  def show; end
+
+  def new
+    @tasks = Task.new
+    redirect_to tasks_path
+  end
+
+  def create
+    @tasks = Task.new(task_params)
+    @tasks.save
+    redirect_to tasks_path(@tasks)
+  end
+
+  def edit; end
+
+  def update
+    @tasks.update(task_params)
+    redirect_to tasks_path(@tasks)
+  end
+
+  def destroy
+    @tasks.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set_task
+    @tasks = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 end
